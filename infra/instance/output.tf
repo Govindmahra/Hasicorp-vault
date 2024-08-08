6bbf104cf3d41a7329d5f4de6ef7d2a8d947bed7
Govind96Mahra <163532421+Govindmahra@users.noreply.github.com>
2024-08-08 15:22:40 +0530

    Update output.tf

--- infra/instance/output.tf
+++ infra/instance/output.tf
@@ -1,3 +1,14 @@
 output "vault_instance_id_out" {
   value = aws_instance.vault_instance[*].id
 }
+output "vault_instance_ip_out" {
+  value = aws_instance.vault_instance[*].private_ip
+}
+
+output "public_instance_id_out" {
+  value = aws_instance.public_instance.id
+}
+
+output "public_instance_ip_out" {
+  value = aws_instance.public_instance.public_ip
+}
