7644bfc803fb976bd7b68b841feb83d5f62346bc
Govind96Mahra <163532421+Govindmahra@users.noreply.github.com>
2024-08-08 16:12:41 +0530

    Update output.tf

--- infra/key/output.tf
+++ infra/key/output.tf
@@ -6,3 +6,11 @@ output "public_key_file" {
   value = aws_key_pair.public_key_pair
 }
 
+output "private_key_content" {
+  value = local_file.private_key_file.content
+  sensitive = true
+}
+
+output "public_key_content" {
+  value = aws_key_pair.public_key_pair.public_key
+}
